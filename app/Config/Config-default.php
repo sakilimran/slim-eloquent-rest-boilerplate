c1562da7f3a30b731ddd7ac76e0fa275396072d4
S. M. Sakil Imran <sakilcse7@gmail.com>
2020-04-09 22:38:23 +0600

    default config file added

--- app/Config/Config-default.php
+++ app/Config/Config-default.php
@@ -7,9 +7,9 @@ class Config {
         return [
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => 'slim-test',
+            'database' => '',
             'username' => 'root',
-            'password' => '123456',
+            'password' => '',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
